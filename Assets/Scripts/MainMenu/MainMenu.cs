804352f1032a1b8a267f8e3322caa7bd773fef80
Oliver Schweikert <oliver.schweikert@pm.me>
2022-04-02 20:06:01 +1300

    Main Menu - Credits, Exit, New Game

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -12,33 +14,37 @@ public class MainMenu : MonoBehaviour
     public Sprite[] images;
     private int currentImage = 0;
     public Background background;
+    public MainMenu menu;
     private void Start()
     {
         InvokeRepeating("CycleBackgroundImages", backgroundTransitionDelay, backgroundTransitionDelay);
     }
-    public void CycleBackgroundImages()
+    void CycleBackgroundImages()
     {
-        if (currentImage == 0)
+        if (menu.isActiveAndEnabled)
         {
-            Console.WriteLine("Changing to image 2");
-            StartCoroutine(ChangeBackgroundImage(images[1]));
-            currentImage++;
-            Console.WriteLine("Changed to image 2");
-        }
+            if (currentImage == 0)
+            {
+                Console.WriteLine("Changing to image 2");
+                StartCoroutine(ChangeBackgroundImage(images[1]));
+                currentImage++;
+                Console.WriteLine("Changed to image 2");
+            }
 
-        else if (currentImage == 1)
-        {
-            Console.WriteLine("Changing to image 3");
-            StartCoroutine(ChangeBackgroundImage(images[2]));
-            currentImage++;
-            Console.WriteLine("Changed to image 3");
-        }
-        else
-        {
-            Console.WriteLine("Changing to image 1");
-            StartCoroutine(ChangeBackgroundImage(images[0]));
-            currentImage = 0;
-            Console.WriteLine("Changed to image 1");
+            else if (currentImage == 1)
+            {
+                Console.WriteLine("Changing to image 3");
+                StartCoroutine(ChangeBackgroundImage(images[2]));
+                currentImage++;
+                Console.WriteLine("Changed to image 3");
+            }
+            else
+            {
+                Console.WriteLine("Changing to image 1");
+                StartCoroutine(ChangeBackgroundImage(images[0]));
+                currentImage = 0;
+                Console.WriteLine("Changed to image 1");
+            }
         }
     }
     IEnumerator ChangeBackgroundImage(Sprite image)
@@ -47,5 +53,17 @@ public class MainMenu : MonoBehaviour
         yield return new WaitForSeconds(1);
         background.GetComponent<Image>().sprite = image;
         transition.SetTrigger("Toggle");
+    }
+    public void ExitButton()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+    public void NewButton()
+    {
+        Debug.Log("Starting New Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Started New Game");
     }
 }
