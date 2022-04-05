17f1cea132615eaab1a0dfd5038ac154dba708a8
Oliver Schweikert <oliver.schweikert@pm.me>
2022-04-06 09:51:04 +1200

    Aim with right click

--- Assets/Scripts/Level/CharacterMovement.cs
+++ Assets/Scripts/Level/CharacterMovement.cs
@@ -1,30 +1,87 @@
 using UnityEngine;
-
 public class CharacterMovement : MonoBehaviour
 {
+    [Header("Attributes:")]
     public float moveSpeed = 5f;
+    public float aimSpeed = 1f;
+
+    [Space]
+    [Header("Statistics:")]
+    public Vector2 movement;
+    public Vector2 mousePosition;
+    public bool aiming;
+
+    [Space]
+    [Header("References:")]
     public Rigidbody2D rb;
-    Vector2 movement;
-    Vector2 mousePosition;
     public Camera cam;
     public Animator animator;
+    public GameObject crosshair;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
     private void Update()
+    {
+        ProcessInput();
+        if (!PauseMenu.gameIsPaused)
+        {
+            AnimateCharacter();
+        }
+    }
+    private void FixedUpdate()
+    {
+        SetPositions();
+    }
+    private void SetPositions()
+    {
+        if (aiming)
+        {
+            rb.MovePosition(rb.position + movement.normalized * aimSpeed * Time.fixedDeltaTime);
+        }
+        if (!aiming)
+        {
+            rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        }
+        crosshair.transform.position = mousePosition;
+    }
+    private void ProcessInput()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDirection = mousePosition - rb.position;
-        animator.SetFloat("MouseX", lookDirection.x);
-        animator.SetFloat("MouseY", lookDirection.y);
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        aiming = Input.GetKey(KeyCode.Mouse1);
     }
-    private void FixedUpdate()
+    private void AnimateCharacter()
     {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        LookAnimation();
+        RunAnimation();
+        AimAnimation();
 
-        // Vector2 lookDirection = mousePosition - rb.position;
-        // float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        void AimAnimation()
+        {
+            if (aiming)
+            {
+                animator.SetFloat("Speed", 0);
+            }
+        }
+
+        void RunAnimation()
+        {
+            if (!aiming)
+            {
+                animator.SetFloat("Horizontal", movement.x);
+                animator.SetFloat("Vertical", movement.y);
+                animator.SetFloat("Speed", movement.sqrMagnitude);
+            }
+        }
+
+        void LookAnimation()
+        {
+            Vector2 lookDirection = mousePosition - rb.position;
+            animator.SetFloat("MouseX", lookDirection.x);
+            animator.SetFloat("MouseY", lookDirection.y);
+        }
     }
 }
